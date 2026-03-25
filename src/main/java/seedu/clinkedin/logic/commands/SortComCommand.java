45a5fdc1f1d035f7015012341d7681ac9ac9ab2d
savagexr3 <xrongleee@gmail.com>
2026-03-25 14:27:30 +0800

    Fix checkstyle

--- src/main/java/seedu/clinkedin/logic/commands/SortComCommand.java
+++ src/main/java/seedu/clinkedin/logic/commands/SortComCommand.java
@@ -3,6 +3,7 @@ package seedu.clinkedin.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.logging.Logger;
+
 import seedu.clinkedin.commons.core.LogsCenter;
 import seedu.clinkedin.commons.util.ToStringBuilder;
 import seedu.clinkedin.model.Model;
@@ -11,15 +12,12 @@ import seedu.clinkedin.model.Model;
  * Sorts the displayed contact list alphabetically by company name, case-insensitively.
  */
 public class SortComCommand extends Command {
-
-    private static final Logger logger = LogsCenter.getLogger(SortComCommand.class);
     public static final String COMMAND_WORD = "sortcom";
-
+    public static final String MESSAGE_SUCCESS = "Contacts sorted by company name.";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts all displayed contacts by company name "
             + "alphabetically (case-insensitive).\n"
             + "Example: " + COMMAND_WORD;
-
-    public static final String MESSAGE_SUCCESS = "Contacts sorted by company name.";
+    private static final Logger logger = LogsCenter.getLogger(SortComCommand.class);
 
     @Override
     public CommandResult execute(Model model) {
