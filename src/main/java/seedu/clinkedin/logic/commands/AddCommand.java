b174d2a09ef96e62403b5ff495592ca4355e08ff
jun hui <patrickstarjh@gmail.com>
2026-04-11 23:02:05 +0800

    Add spacing between link and tag

--- src/main/java/seedu/clinkedin/logic/commands/AddCommand.java
+++ src/main/java/seedu/clinkedin/logic/commands/AddCommand.java
@@ -46,7 +46,7 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_COMPANY + "Google "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_LINK + "https://www.linkedin.com/feed/"
+            + PREFIX_LINK + "https://www.linkedin.com/feed/ "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
